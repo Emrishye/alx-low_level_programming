390d22da0b4d9b6f1120c4f37a56744cc14e159e
emran <abumeryem04@gmail.com>
2023-02-16 13:02:48 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+
+
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * Returns:  Always 0.
  */
 
+
 int main(void)
+
 
 { 
 	puts("\"Programming is like building a multilingual puzzle");
 	returns (0);
+
 }
