0f7dd114b3b2eca6266097267c275a655e30fb8b
emran <abumeryem04@gmail.com>
2023-03-21 00:43:27 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - gives info about a dog
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
